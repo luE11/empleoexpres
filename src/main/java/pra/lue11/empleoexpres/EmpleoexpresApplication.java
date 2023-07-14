e2b71de5720c35b7e01d191ad3fb32f99ecd4306
Luis Martínez <luisedu555@gmail.com>
2023-07-14 14:36:07 -0500

    base structure

---
+++ src/main/java/pra/lue11/empleoexpres/EmpleoexpresApplication.java
@@ -0,0 +1,13 @@
+package pra.lue11.empleoexpres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleoexpresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleoexpresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pra/lue11/empleoexpres/EmpleoexpresApplicationTests.java
@@ -0,0 +1,13 @@
+package pra.lue11.empleoexpres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleoexpresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
